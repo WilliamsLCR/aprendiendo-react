f3e3c01cabe21b5d77a582a52bb9074a37f1b2d5
Williams JJ <williamsjimenezjulian@outlook.com>
2025-05-15 14:51:37 -0700

    Agregado de dos actividades de google maps

--- src/pages/GoogleMapas.jsx
+++ src/pages/GoogleMapas.jsx
@@ -1,10 +1,9 @@
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 
 const containerStyle = {
   width: '100%',
-  height: '400px'
+  height: '800px'
 };
 
 const center = {
@@ -15,7 +14,7 @@ const center = {
 
 export default function Mapa() {
   return (
-    <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_MAPS_API_KEY}>
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
         <Marker position={center} />
       </GoogleMap>
--- src/pages/GoogleMapasDirecciones.jsx
+++ src/pages/GoogleMapasDirecciones.jsx
@@ -1,5 +1,64 @@
-export default function GoogleMapasDirecciones() {
-    return(
-        <h1>Google Maps generar direcciones</h1>
-    );
-}
+import React, { useState } from 'react';
+import { GoogleMap, Marker, DirectionsService, DirectionsRenderer, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '100%',
+    height: '800px'
+};
+
+const origen = { lat: 19.4326, lng: -99.1332 };
+const destino = { lat: 19.7036, lng: -99.1013 };
+
+const GoogleMapasDirecciones = () => {
+    const { isLoaded } =  useJsApiLoader({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+        libraries: ['places']
+    });
+
+    const [directions, setDirections] = useState(null);
+
+    const directionsCallback = (response) => {
+        if (response !== null) {
+            if (response.status === 'OK') {
+                setDirections(response);
+            } else {
+                console.log('Error al obtener las direcciones:', response);
+            }
+        }
+    };
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={origen}
+            zoom={10}
+        >
+
+        <DirectionsService
+            options={{
+                destination: destino,
+                origin: origen,
+                travelMode: 'DRIVING'
+            }}
+            callback={directionsCallback}
+        />
+
+        { directions && (
+            <DirectionsRenderer
+                options={{
+                    directions: directions
+                }}
+            />
+        )}
+
+        <Marker position={origen} label="A" />
+        <Marker position={destino} label="B" />
+
+        </GoogleMap>
+
+        ) : (
+            <div>Cargado mapa...</div>
+        );
+};
+
+export default GoogleMapasDirecciones;
