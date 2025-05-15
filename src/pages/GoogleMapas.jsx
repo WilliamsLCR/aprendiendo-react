6b75f575db0d77dffa8f984f871a8145af6756b2
Williams JJ <williamsjimenezjulian@outlook.com>
2025-05-15 00:33:53 -0700

    Implementacion de google maps

--- src/pages/GoogleMapas.jsx
+++ src/pages/GoogleMapas.jsx
@@ -1,5 +1,24 @@
-export default function GoogleMapas() {
-    return(
-        <h1>Google Maps</h1>
-    );
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 23.2313686,
+  lng: -106.4265038
+
+};
+
+export default function Mapa() {
+  return (
+    <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
+  );
 }
